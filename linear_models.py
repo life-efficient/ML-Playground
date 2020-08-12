44baa8bb5e04db6a68d23983e2697d0e2a233d20
Francesco <fra.meloni.ca1@gmail.com>
2020-08-12 21:51:20 +0100

    created loss and gridsearch methods fro linreg

--- linear_models.py
+++ linear_models.py
@@ -12,3 +12,28 @@ class LinearRegression:
     def update_params(self, new_w, new_b):
         self.w = new_w ## set this instance's weights to the new weight value passed to the function
         self.b = new_b ## do the same for the bias
+
+    def loss(self, X, Y, y_hat= None):
+        if y_hat is None:
+            y_hat = self(X)
+        diff = y_hat - Y
+        squares = np.squared(diff)
+        mean = np.mean(squares)
+        return mean
+
+    def grid_search(self, X, Y, limit= 20, step=1):
+
+        best_weights = 0 ## no best weight found yet
+        best_bias = 0 ## no best bias found yet
+        lowest_cost = float('inf') ## initialize it very high (how high can it be?)
+        for w in range(-limit, limit, step):
+            for b in range(-limit, limit, step): ## try this many different parameterisations
+                self.update_params(w, b) ## update our model with these random parameters
+                y_hat = self(X) ## make prediction
+                cost = self.loss(y_hat, Y) ## calculate loss
+                if cost < lowest_cost: ## if this is the best parameterisation so far
+                    lowest_cost = cost ## update the lowest running cost to the cost for this parameterisation
+                    best_weights = w ## get best weights so far from the model
+                    best_bias = b ## get best bias so far from the model
+        print('Lowest cost of', lowest_cost, 'achieved with weight of', best_weights, 'and bias of', best_bias)
+        return best_weights, best_bias ## return the best weight and best bias
