8e5f2948701fd1e71fce23b5b3f2843f59881f4a
Farhad Saiepour <fsaie74@yahoo.co.uk>
2020-08-14 18:26:00 +0100

    The speeling has been resolved

--- Standardising_Data.py
+++ Standardising_Data.py
@@ -4,7 +4,7 @@ def standardize_data(dataset):
 
     mean, std = np.mean(dataset, axis=0), np.std(dataset, axis=0) ## get mean and standard deviation of dataset
     standardized_dataset  = (dataset-mean)/std
-    print("Here is the shape of your standardised Data : ", stanadrdized_dataset.shape)
+    print("Here is the shape of your standardised Data : ", standardized_dataset.shape)
 
     return standardized_dataset
 
