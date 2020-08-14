a1152cc9dd1cbcd723167ee3fb9a6a4a60ddaaee
EvgenyDyshlyuk <evgeny.dyshlyuk@gmail.com>
2020-08-14 19:17:12 +0100

    updated function

---
+++ batch_together.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def batch_together(examples):
+    '''
+    Function that batch examples together using vstack
+    input: tuple or list of examples
+    output: list of vertically stacked examples
+    ''' 
+    return np.vstack(examples)
+
+
+'''
+examples = ([[1,2,3],[4,5,6],[7,8,9]],
+            [[11,12,13],[14,15,16],[17,18,19]])
+print(batch_together(examples))
+
+examples = [[1],
+            [2]]
+print(batch_together(examples))
+'''
--- function_to_batch_together_examples.py
+++
@@ -1,1 +0,0 @@
-print('This is a function to batch together examples')
